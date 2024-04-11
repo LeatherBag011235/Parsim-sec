cd90933570a86a7544b81c10d80153e3b5acfaa8
FyodorSologub <141565918+FyodorSologub@users.noreply.github.com>
2024-04-11 17:38:20 +0300

    removed unused code

--- src/converter/index.py
+++
@@ -1,8 +0,0 @@
-from utils import *
-
-def convert_files():
-    obj = get_object()
-    
-    for key, items in obj.items():
-        texts = [process_text(key, item) for item in items]
-        save_to_parquet(key, texts)
--- src/downloader/index.py
+++
@@ -1,6 +0,0 @@
-from utils import download_file
-
-def download_files(company_links_object):
-    for key in company_links_object.keys():
-        for item_obj in company_links_object[key]:
-            download_file(key, item_obj['page_link'], item_obj['filed_date'], item_obj['reporting_for'])
