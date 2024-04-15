10f51b19eceffb80ccc73d380a74cdda8dda2a61
LeatherBag011235 <93046087+LeatherBag011235@users.noreply.github.com>
2024-04-15 17:09:25 +0300

    main.py: example of import module is added

--- src/main.py
+++ src/main.py
@@ -1,15 +1,17 @@
-from utils import *
+# from utils import *
+from downloader.index import download_files
 import time
 
 start_time = time.time()
 
 def main():
-    driver = createDriver()
-    parse_all_links(driver)
-    download_files_2()
-    convert_files()
-    get_object()
-    print(get_date('./raw_files/Apple%2520Inc.%2520(AAPL)%2520(CIK%25200000320193)/2020-05-01_2020-03-28'))
+    # driver = createDriver()
+    # parse_all_links(driver)
+    # download_files_2()
+    # convert_files()
+    # get_object()
+    download_files([1, 2, 3])
+    # print(get_date('./raw_files/Apple%2520Inc.%2520(AAPL)%2520(CIK%25200000320193)/2020-05-01_2020-03-28'))
 if __name__ == '__main__':
     main()
 
