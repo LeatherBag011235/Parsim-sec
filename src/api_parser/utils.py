e87d39ce35c82faa2077f8b9a2765de9e3ec5a72
LeatherBag011235 <93046087+LeatherBag011235@users.noreply.github.com>
2024-04-17 16:42:23 +0300

    no hope in html structure of reports

--- src/api_parser/utils.py
+++ src/api_parser/utils.py
@@ -10,8 +10,6 @@ def get_random_headers():
     return headers
 
 
-import requests
-from bs4 import BeautifulSoup
 
 def download_report(cik, year, qtr, report_type):
     base_url = "https://www.sec.gov/Archives/edgar/full-index"
@@ -27,18 +25,18 @@ def download_report(cik, year, qtr, report_type):
 
         
         if (len(parts) > 4):
-            print(parts)
-            print(parts[3], parts[4])
-            form_type, company_cik, date_filed, filename = parts[2], parts[4], parts[3], parts[4]
-            if company_cik == cik and form_type in report_type:
-                # Construct the URL to download the report
-                report_url = f"https://www.sec.gov/Archives/{filename}"
-                report_response = requests.get(report_url)
-                
-                # Save the report
-                with open(f"{company_cik}_{form_type}_{date_filed}.txt", 'wb') as f:
-                    f.write(report_response.content)
-                print(f"Downloaded {form_type} for {company_cik} filed on {date_filed}")
+            if (parts[2] == '10-Q') | (parts[2] == '10-K'):
+                print(parts)
+                form_type, company_cik, date_filed, filename = parts[2], parts[4], parts[3], parts[4]
+                if company_cik == cik and form_type in report_type:
+                    # Construct the URL to download the report
+                    report_url = f"https://www.sec.gov/Archives/{filename}"
+                    report_response = requests.get(report_url)
+                    
+                    # Save the report
+                    with open(f"{company_cik}_{form_type}_{date_filed}.txt", 'wb') as f:
+                        f.write(report_response.content)
+                    print(f"Downloaded {form_type} for {company_cik} filed on {date_filed}")
 
 # Example usage
 cik = '0000320193'  # Apple's CIK
