1586cbf056ccce2394924c0c1138aaeda30862ed
LeatherBag011235 <93046087+LeatherBag011235@users.noreply.github.com>
2024-05-19 00:16:19 +0300

    base regression analysis completed

--- src/converter_api/index.py
+++ src/converter_api/index.py
@@ -3,8 +3,10 @@ from .utils import *
 def convert_files(files_to_convert):
 
     output_dir_for_get, output_dir_for_save = spesify_dir(files_to_convert)
+    print('dirs have been specified')
 
     dict_with_all_companies = extract_strings(output_dir_for_get)
+    print("strings have been extracted")
 
     for company_name, company_dict in dict_with_all_companies.items():
         print(company_name)
